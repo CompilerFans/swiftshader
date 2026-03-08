eef91e29ea7f4a7412fb716518e83f3dd0e3105e
cjxu <123421156@qq.com>
2026-03-08 17:58:33 +0800

    tests: add present adapter capture test
    Change-Id: I2ae53fbedaebed5de3e37b64ecebd026b926698b

---
+++ tests/VulkanUnitTests/PresentAdapterCaptureTests.cpp
@@ -0,0 +1,40 @@
+#include "Backend/PresentAdapter.hpp"
+#include "Device.hpp"
+#include "Driver.hpp"
+
+#include "gtest/gtest.h"
+
+class PresentAdapterCaptureTest : public testing::Test
+{
+protected:
+	static Driver driver;
+
+	static void SetUpTestSuite()
+	{
+		ASSERT_TRUE(driver.loadSwiftShader());
+	}
+
+	static void TearDownTestSuite()
+	{
+		driver.unload();
+	}
+};
+
+Driver PresentAdapterCaptureTest::driver;
+
+#if SWIFTSHADER_ENABLE_CUSTOM_GPU_BACKEND
+TEST_F(PresentAdapterCaptureTest, CustomPresentAdapterCaptureApiWorks)
+{
+	backend::resetPresentAdapterCapture();
+	backend::ResourceStateTracker tracker;
+	auto adapter = backend::createPresentAdapter();
+	ASSERT_NE(adapter, nullptr);
+	adapter->acquire(tracker, 21);
+	adapter->present(tracker, 21);
+	const auto &capture = backend::lastPresentAdapterCapture();
+	EXPECT_EQ(capture.acquireCount, 1u);
+	EXPECT_EQ(capture.presentCount, 1u);
+	EXPECT_EQ(capture.lastAcquireImageId, 21u);
+	EXPECT_EQ(capture.lastPresentImageId, 21u);
+}
+#endif
